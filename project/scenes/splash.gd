5663d341873aa38263776888bfb70688a6da134b
Tracteur Blindé <tracteur@chiral.dev>
2023-11-10 19:54:51 -0800

    Placeholder menu

--- project/scenes/splash.gd
+++ project/scenes/splash.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _ready():
 	$AnimationPlayer.play("fade_godot")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "fade_godot":
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
