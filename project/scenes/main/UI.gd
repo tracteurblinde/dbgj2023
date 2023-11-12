c8afc15ccf652f91c914754ae7597f743935ec34
Tracteur Blindé <tracteur@chiral.dev>
2023-11-11 22:05:56 -0800

    Character asset

--- project/scenes/main/UI.gd
+++ project/scenes/main/UI.gd
@@ -1,5 +1,6 @@
 extends Control
 
+signal intro_finished
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +14,4 @@ func _process(_delta):
 
 func _on_animation_player_animation_finished(anim_name:StringName):
 	if anim_name == "intro":
-		pass
+		emit_signal("intro_finished")
