c3cc9572a367a7f2099f00539109d9b24f862404
Tracteur Blindé <tracteur@chiral.dev>
2023-11-14 12:41:28 -0800

    New title sequence

--- project/scenes/main/UI.gd
+++ project/scenes/main/UI.gd
@@ -1,6 +1,30 @@
 extends Control
 
 signal intro_finished
+signal anykey_pressed
+
+var has_pressed_anykey: bool = false
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			_on_anykey_pressed()
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed:
+			_on_anykey_pressed()
+
+
+func _on_anykey_pressed():
+	if has_pressed_anykey:
+		return
+
+	has_pressed_anykey = true
+	emit_signal("anykey_pressed")
+	$AnimationPlayer.play("fade_intro")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,5 +38,5 @@ func _process(_delta):
 
 
 func _on_animation_player_animation_finished(anim_name: StringName):
-	if anim_name == "intro":
+	if anim_name == "fade_intro":
 		emit_signal("intro_finished")
