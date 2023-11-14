333840edda3750c43c98ff6fe00c15a9e28f8637
Tracteur Blindé <tracteur@chiral.dev>
2023-11-14 14:14:45 -0800

    Change around mouse capture

--- project/scenes/Player/SprintArmPivot.gd
+++ project/scenes/Player/SprintArmPivot.gd
@@ -11,20 +11,38 @@ const CAMERA_BLEND: float = 0.05
 @onready var camera: Camera3D = $SpringArm3D/Camera3D
 var player: CharacterBody3D = null
 
+var is_mouse_captured: bool = false
+
 
 func _ready():
 	player = get_parent()
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			if not is_mouse_captured:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+				is_mouse_captured = true
+			else:
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+				is_mouse_captured = false
+
 	if !player.is_active():
 		return
 
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and is_mouse_captured:
 		rotate_y(-event.relative.x * 0.005)
 		spring_arm.rotate_x(-event.relative.y * 0.005)
 		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI / 4, PI / 4)
+
+
+func _input(_event):
+	# If ui_cancel is pressed, release the mouse if it's captured
+	if Input.is_action_just_pressed("ui_cancel"):
+		if is_mouse_captured:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			is_mouse_captured = false
 
 
 func _physics_process(_delta):
